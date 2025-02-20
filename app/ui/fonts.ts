2c526e5ce1bb2d2b76c45c35f1e9686ed7842d97
alex <azhang1304@gmail.com>
2025-02-20 19:52:53 +1100

    Imported fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Schibsted_Grotesk } from 'next/font/google'
+import Candu  from 'next/font/local'
+
+export const schibstedGrotesk = Schibsted_Grotesk({
+    subsets: ['latin'],
+    display: 'swap', 
+    });
+
+export const candu = Candu({
+    src: '../../public/fonts/Candu.woff2',
+    display: 'swap',
+    });
+
+
